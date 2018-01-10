d19a56303aa444c33e92d6d636003135297c5968
Maxim Ivanov <omaxphp@yandex.ru>
2018-01-10 23:06:12 +0300

    fix(index.ts): export all interfaces

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { ClientLogger } from "./src/logger";
 
-export { LoggerLevel, LoggerColors, LoggerConfig, LoggerLabels } from './src/logger.interfaces';
-export { ClientLogger } from './src/logger';
+export * from './src/logger.interfaces';
+export * from './src/logger';
 export const logger = new ClientLogger();
