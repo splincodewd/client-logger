a333b6c5c95a7703bc64f9e9d0567d4a04a150c7
splincode <splincodewd@yandex.ru>
2018-04-07 15:25:42 +0300

    ci(repo): fix problem builds for github-pages

--- aio/deploy.sh
+++ aio/deploy.sh
@@ -4,13 +4,13 @@ echo 'Deploy run...'
 
 # config
 echo 'Configuration...'
-git config --global user.email "omaxphp@yandex.ru"
-git config --global user.name "Travis CI"
+#git config --global user.email "omaxphp@yandex.ru"
+#git config --global user.name "Travis CI"
 
 # deploy
 echo 'Publishing...'
 cd aio && cd output
 git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+GIT_TRACE=1 git add .
+GIT_TRACE=1 git commit -m "Deploy to Github Pages"
+GIT_TRACE=1 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
