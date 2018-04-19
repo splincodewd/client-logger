2c493f1f9a4cc2d79f0818865b85a9ab1d06f190
mivanov <mivanov@cinimex.ru>
2018-04-19 14:12:35 +0300

    ci(demo): add stackblitz demo

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -7,10 +7,22 @@ echo 'Configuration...'
 git config --global user.email "omaxphp@yandex.ru"
 git config --global user.name "Travis CI"
 
-# deploy
-echo 'Publishing...'
+# deploy github pages
+echo 'Github pages publishing...'
 cd demo && cd dist
 git init
 GIT_TRACE=1 git add .
 GIT_TRACE=1 git commit -m "Deploy to Github Pages"
 GIT_TRACE=1 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+
+# deploy stackblitz pages
+echo 'Stackblitz publishing...'
+cd ..
+rm -rf dist
+git init
+GIT_TRACE=1 git add .
+GIT_TRACE=1 git commit -m "Deploy to stackblitz"
+GIT_TRACE=1 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:stackblitz > /dev/null 2>&1
+
+
+
