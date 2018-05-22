2c19d29c3439727ee892b1d4eebd4100703a4d0c
mivanov <mivanov@cinimex.ru>
2018-05-22 12:57:47 +0300

    ci(demo): dist deploy

---
+++ demo/dist-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 'Deploy dist run...'
+
+# config
+echo 'Configuration...'
+git config --global user.email "omaxphp@yandex.ru"
+git config --global user.name "Travis CI"
+
+# deploy github pages
+echo 'Github dist publishing...'
+npm run prepublishOnly
+cd lib
+git init
+GIT_TRACE=1 git add .
+GIT_TRACE=1 git commit -m "Deploy to dist"
+GIT_TRACE=1 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:dist > /dev/null 2>&1
