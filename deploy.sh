8e7e833afdd27f0a6c93ba0aaa4971cfa64c3b6e
Travis CI <omaxphp@yandex.ru>
2018-05-24 16:22:47 +0300

    change repo problem with code sandbox #3

--- deploy.sh
+++ deploy.sh
@@ -15,22 +15,23 @@ function git_config() {
   git config --global user.name "Travis CI"
 }
 
-function git_init_push() {
-  echo "https://${GITHUB_TOKEN}@github.com/$1.git" master:$2
-  # initial
-  git init
-  GIT_TRACE=1 git add .
-  GIT_TRACE=1 git commit -m "Deploy to Github"
-  GIT_TRACE=1 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/$1.git" master:$2 > /dev/null 2>&1
+function git_commit_website_files() {
+  git checkout -b $1
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
+function git_push() {
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/$1.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages $2
+}
 
 git_config $email
 
-cp .travis.yml demo/
-
 cd $current && cd demo/dist
-git_init_push $GITHUB_REPO $DEPLOY_TARGET_REPO_GH_PAGES
+git_commit_website_files $DEPLOY_TARGET_REPO_GH_PAGES
+git_push $GITHUB_REPO $DEPLOY_TARGET_REPO_GH_PAGES
 
 cd $current && cd demo/
+git_commit_website_files $GITHUB_REPO
 git_init_push $GITHUB_REPO $DEPLOY_TARGET_REPO_DIST
